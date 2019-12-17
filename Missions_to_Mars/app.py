8030ceb66b57e73a16ca6497b5a088573b586ce4
whysoq520 <53708390+whysoq520@users.noreply.github.com>
2019-12-17 17:58:06 -0500

    update

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,37 +1,30 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_costa
+import PyMongo
+import scrape_surfing
 
-# Create an instance of Flask
+
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/surfing_app")
 
 
-# Route to render index.html template using data from Mongo
-@app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", vacation=destination_data)
+@app.route('/')
+def index():
+    surfing = mongo.db.surfing.find_one()
+    return render_template('index.html', surfing=surfing)
 
 
-# Route that will trigger the scrape function
-@app.route("/scrape")
+@app.route('/scrape')
 def scrape():
-
-    # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
+    surfing = mongo.db.surfing
+    data = scrape_surfing.scrape()
+    surfing.update(
+        {},
+        data,
+        upsert=True
+    )
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
