f61ec39e2f27ca8b48f547d076f725a4fa375725
whysoq520 <53708390+whysoq520@users.noreply.github.com>
2019-12-22 21:57:23 -0500

    yeah. very close

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars,time
+import mars_scrape,time
 
 app = Flask(__name__)
 
@@ -11,14 +11,14 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_db")
 
 @app.route('/')
 def index():
-    mars = mongo.db.mars_collection.find()
+    mars = mongo.db.mars_collection.find_one()
     return render_template('index.html', mars=mars)
 
 
 @app.route('/scrape')
 def scrape():
     # Run the scrape function
-    mars_info= scrape_mars.scrape()
+    mars_info= mars_scrape.scrape()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.mars_collection.update({}, mars_info, upsert=True)
--- table/app.py
+++ table/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import table_scrape
+import scrape_mars,time
 
 app = Flask(__name__)
 
@@ -11,17 +11,17 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_db")
 
 @app.route('/')
 def index():
-    mars = mongo.db.table_collection.find()
+    mars = mongo.db.mars_collection.find_one()
     return render_template('index.html', mars=mars)
 
 
 @app.route('/scrape')
 def scrape():
     # Run the scrape function
-    mars_info= table_scrape.scrape()
+    mars_info= scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.table_collection.update({}, mars_info, upsert=True)
+    mongo.db.mars_collection.update({}, mars_info, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
