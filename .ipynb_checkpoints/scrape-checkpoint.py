b812937e46ab5b242f97c06abcb3640b40902a0d
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-05-27 21:52:53 -0400

    reviewed and update my scrape code

---
+++ .ipynb_checkpoints/scrape-checkpoint.py
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Import Splinter, BeautifulSoup, Regular Expression, and Time\n",
+    "from splinter import Browser\n",
+    "from bs4 import BeautifulSoup\n",
+    "import re\n",
+    "import time"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "'which' is not recognized as an internal or external command,\n",
+      "operable program or batch file.\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Path to chromedriver\n",
+    "!which chromedriver"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.7.4 64-bit ('super': virtualenv)",
+   "language": "python",
+   "name": "python37464bitsupervirtualenve687e24a3a4141a8b6a9e2b32ae60551"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
